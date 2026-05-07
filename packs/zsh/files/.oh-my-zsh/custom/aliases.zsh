8d856c6fadd3db4576ce4db1058e0cd2ce1e0ff2
HASHTAGTB <hxshtxgtb@gmail.com>
2026-05-07 16:35:18 +0200

    backup

---
+++ packs/zsh/files/.oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,38 @@
+# Put files in this folder to add your own custom functionality.
+# See: https://github.com/ohmyzsh/ohmyzsh/wiki/Customization
+#
+# Files in the custom/ directory will be:
+# - loaded automatically by the init script, in alphabetical order
+# - loaded last, after all built-ins in the lib/ directory, to override them
+# - ignored by git by default
+#
+# Example: add custom/shortcuts.zsh for shortcuts to your local projects
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+
+alias update="sudo pacman -Syu"
+alias uninstall="sudo pacman -Rsn"
+alias install="sudo pacman -S"
+alias please="sudo"
+alias cleanup="sudo pacman -Rsn $(pacman -Qtdq)"
+alias c="clear"
+alias inv='nvim $(fzf -m --preview="bat -p --color=always {}")'
+gcom() {
+	git add .
+	git commit -m "$1"
+}
+
+lazyg() {
+	git add .
+	git commit -m "$1"
+	git push
+}
+
+y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	command yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
---
+++ packs/zsh/scripts/install-p10k.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+P10K_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+if [ ! -d "$P10K_DIR" ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$P10K_DIR"
+fi
