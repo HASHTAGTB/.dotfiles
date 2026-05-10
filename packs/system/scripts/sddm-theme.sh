dd1aa018afcfb97be6e9b1196b6e6434c8f66e36
HASHTAGTB <hxshtxgtb@gmail.com>
2026-05-10 12:29:00 +0200

    idk

--- packs/system/scripts/sddm-theme.sh
+++ packs/system/scripts/sddm-theme.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env/ bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/keyitdev/sddm-astronaut-theme/master/setup.sh)"
-sudo sed -i 's|ConfigFile=.*|ConfigFile=Themes/pixel_sakura.conf|' /usr/share/sddm/themes/sddm-astronaut-theme/metadata.desktop
